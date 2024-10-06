483cee436b4293e115d6ca2d835f3f80d5cd838e
debisela <debora.jaeckel@gmail.com>
2024-10-06 23:55:28 +0300

    check that can fetch on frontend from db

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const app = express()
 
 app.use(cors())
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+// app.use(express.urlencoded({extended:true}))
+// app.use(express.json())
 
 
 const PORT = process.env.PORT || 3200
