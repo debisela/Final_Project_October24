9bd5ddceb143d5932a2e2eeaeac0e78d70b4bfd9
debisela <debora.jaeckel@gmail.com>
2024-10-10 14:47:31 +0300

    endpoint get search attendee

---
+++ routes/userRoute.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const { _searchAttendee } = require('../controllers/userController.js')
+
+
+const router = express.Router()
+
+//search for attendees
+// router.get('/search/:query')
+router.get('/attendees', _searchAttendee)
+
+module.exports = router
