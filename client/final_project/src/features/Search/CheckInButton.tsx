b9f15f05dd5cccf9f1dfbd2ad8b18e4a226e7e2b
debisela <debora.jaeckel@gmail.com>
2024-10-20 00:52:28 +0300

    separate component for checkin

---
+++ client/final_project/src/features/Search/CheckInButton.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import {toggleCheckIn } from "./state/searchSlice";
+
+interface CheckInToggleProps {
+    attendeeId: number;
+    checkedIn: boolean;
+  }
+
+  const CheckInButton: React.FC<CheckInToggleProps> = ({attendeeId, checkedIn})=>{
+    const dispatch = useDispatch()
+
+    const handleCheckIn = async ()=>{
+        // console.log("id checkin", attendeeId);
+        
+        try {
+          await dispatch(toggleCheckIn(attendeeId)).unwrap();
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      return(
+        <>
+        <button onClick={handleCheckIn}>
+                  {checkedIn ? "Check Out" : "Check In"}
+                  </button>
+        </>
+    )
+  }
+  export default CheckInButton;
