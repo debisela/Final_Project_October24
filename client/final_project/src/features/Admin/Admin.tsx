2c80744aa4c7443e99e56bf807bc77ec45db61bd
debisela <debora.jaeckel@gmail.com>
2024-10-25 00:12:08 +0300

    show all fields with checkbox

--- client/final_project/src/features/Admin/Admin.tsx
+++ client/final_project/src/features/Admin/Admin.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchFields } from "./state/selectSlice";
+import { useFieldsSelector, useFieldsStatus } from "./state/hooks";
+
 
 const Admin: React.FC = ()=>{
+    const fields = useFieldsSelector()
+    const status = useFieldsStatus()
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(fetchFields())
+    }, [dispatch])
+
+
+    if (status === 'loading') return <h2>Loading...</h2>
+  if (status === 'failed') return <h2>Can't get fields...</h2>
     return(
         <>
-        Admin
+        <div>
+            {fields.map((field:string, index:number)=>(
+                <div key={index}>
+                    <input 
+                    type="checkbox" 
+                    id={field.column_name} 
+                    name={field.column_name}/>
+                    <label htmlFor={field.column_name}>{field.column_name}</label>
+                    
+
+                </div>
+            ))}
+            </div>
         </>
     )
 }
