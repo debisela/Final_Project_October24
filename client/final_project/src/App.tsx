55f96313ee5b478505b2df6a61c209d6310c198d
debisela <debora.jaeckel@gmail.com>
2024-10-24 21:57:13 +0300

    navigate

--- client/final_project/src/App.tsx
+++ client/final_project/src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css'
-import Test from './components/Test'
+import { Route, Routes } from 'react-router-dom'
+import IntroPage from './features/Welcome/IntroPage'
 import SearchAttendee from './features/Search/SearchAttendee'
+import Admin from './features/Admin/Admin'
 
 function App() {
   
 
   return (
     <>
-     <SearchAttendee/>
-       
+    <IntroPage/>
+    <Routes>
+      <Route path="/user" element={<SearchAttendee/>}/>
+      <Route path="/admin" element={<Admin/>}/>
+     </Routes>
     </>
   )
 }
