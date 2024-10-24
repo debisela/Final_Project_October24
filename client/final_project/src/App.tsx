f3b57f2b127ff06eb385b30c0b9f41fda7d4fb0d
debisela <debora.jaeckel@gmail.com>
2024-10-24 22:23:56 +0300

    header

--- client/final_project/src/App.tsx
+++ client/final_project/src/App.tsx
@@ -1,16 +1,19 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Header from './features/Header/Header'
 import IntroPage from './features/Welcome/IntroPage'
 import SearchAttendee from './features/Search/SearchAttendee'
 import Admin from './features/Admin/Admin'
 
 function App() {
+  const location = useLocation()
   
 
   return (
     <>
-    <IntroPage/>
+    {location.pathname !=='/' && <Header/>}
     <Routes>
+     <Route path="/" element={<IntroPage/>}/>
       <Route path="/user" element={<SearchAttendee/>}/>
       <Route path="/admin" element={<Admin/>}/>
      </Routes>
